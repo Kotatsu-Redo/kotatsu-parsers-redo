ef8d61e8967a73a6f228ca0ef189c722be1f1344
Razan M. Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-07-19 00:09:48 +0700

    Update Medusascans.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/comicaso/id/Medusascans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/comicaso/id/Medusascans.kt
@@ -1,11 +1,17 @@
 package org.koitharu.kotatsu.parsers.site.comicaso.id
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaParserAuthProvider
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.exception.AuthRequiredException
 import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.comicaso.ComicasoParser
 
 @MangaSourceParser("MEDUSASCANS", "Medusascans", "id", ContentType.HENTAI)
 internal class Medusascans(context: MangaLoaderContext) :
-	ComicasoParser(context, MangaParserSource.MEDUSASCANS, "medusascans.com", pageSize = 16)
+	ComicasoParser(context, MangaParserSource.MEDUSASCANS, "v3.comicaso.pro", pageSize = 20) {
+
+	override val apiSource: String = "medusa"
+
+}
